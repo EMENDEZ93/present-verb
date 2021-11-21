13edc223ec7670e77a2f58171c70dbf5af782e5f
EdwinMendezCeiba <94276167+EdwinMendezCeiba@users.noreply.github.com>
2021-11-21 13:07:46 -0500

    Hojas

--- src/main/java/present/verb/dominio/temas/puerto/repository/GetHojasByExcelAndCorreoRepository.java
+++ src/main/java/present/verb/dominio/temas/puerto/repository/GetHojasByExcelAndCorreoRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface GetHojasByExcelAndCorreoRepository extends JpaRepository<Usuario, Long> {
 
     @Transactional(readOnly = true)
-    @Query(name = "getHojasByExcelAndCorreoQuery")
+    @Query(name = "getHojasByExcelAndCorreo")
     List<Hoja> execute(String excel, String correo);
 
 }
