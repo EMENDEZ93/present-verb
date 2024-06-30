0bda29303e056b5cbe2a211d9894752651562419
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2024-06-30 07:48:37 -0500

    ajuste

---
+++ src/test/java/present/verb/PresentVerbApplicationTest.java
@@ -0,0 +1,13 @@
+package present.verb;
+
+
+import org.junit.Test;
+
+public class PresentVerbApplicationTest {
+
+    @Test
+    public void testMain() {
+        String[] args = {"arg1", "arg2"};
+    }
+
+}
