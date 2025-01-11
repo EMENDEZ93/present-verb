1a2fe915623ab50a90f377b00c16de964eed3036
EdwinDMendezR <61098711+EdwinDMendezR@users.noreply.github.com>
2025-01-11 12:29:52 -0500

    ajustest

--- src/main/java/present/verb/dominio/espacios/trabajo/ObtenerEspacioTrabajo.java
+++ src/main/java/present/verb/dominio/espacios/trabajo/ObtenerEspacioTrabajo.java
@@ -17,7 +17,7 @@ import static java.util.Arrays.stream;
 @Service
 public class ObtenerEspacioTrabajo {
 
-    @Value("classpath:*")
+    @Value("classpath:**")
     private Resource[] resources;
 
     @Autowired
